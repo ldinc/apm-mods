e0c974273feca7a8825b12e88aee82459008cc17
ldinc <drogunov.igor@gmail.com>
2021-01-09 02:23:51 +0300

    Update furnaces.lua

--- apm_bob_rework/lib/override/furnaces.lua
+++ apm_bob_rework/lib/override/furnaces.lua
@@ -5,6 +5,20 @@ if apm.bob_rework.lib.override.list == nil then apm.bob_rework.lib.override.list
 require('lib.enities.base')
 require('lib.tier.base')
 
-apm.bob_rework.lib.override.furnaces = function ()
+local buildEFurnace = function (recipe, tier)
     
+end
+
+apm.bob_rework.lib.override.furnaces = function ()
+    local recipe = 'electric-mixing-furnace'
+    apm.lib.utils.recipe.ingredient.remove_all(recipe)
+    apm.lib.utils.recipe.ingredient.mod(recipe, apm.bob_rework.lib.entities.steel, 10)
+    apm.lib.utils.recipe.ingredient.mod(recipe, apm.bob_rework.lib.entities.stoneBrick, 10)
+    apm.lib.utils.recipe.ingredient.mod(recipe, apm.bob_rework.lib.entities.steelPipe, 5)
+    apm.lib.utils.recipe.ingredient.mod(recipe, apm.bob_rework.lib.entities.logicAdvanced, 5)
+
+    local recipe = 'electric-chemical-mixing-furnace'
+    apm.lib.utils.recipe.ingredient.mod(recipe, apm.bob_rework.lib.entities.advancedMachineFrame, 0)
+    local recipe = 'electric-chemical-mixing-furnace-2'
+    apm.lib.utils.recipe.ingredient.mod(recipe, apm.bob_rework.lib.entities.advancedMachineFrame, 0)
 end
