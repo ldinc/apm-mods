72f389297337754eac78ce385d2dbe11a8a3b73a
ldinc <drogunov.igor@gmail.com>
2021-02-05 00:59:49 +0300

    Update bearing.lua

--- apm_bob_rework/lib/enities/new/bearing.lua
+++ apm_bob_rework/lib/enities/new/bearing.lua
@@ -3,7 +3,7 @@ if apm.bob_rework.lib.entities == nil then apm.bob_rework.lib.entities = {} end
 
 require('lib.enities.gearingAndBearings')
 
-apm.bob_rework.lib.entities.genBearing = function (name, base, ball, tint)
+apm.bob_rework.lib.entities.genBearing = function (name, base, ball, tint, handmade)
     local ico = {
         icon = "__apm_bob_rework_ldinc__/graphics/icons/bearing.png",
         icon_size = 32,
@@ -45,13 +45,43 @@ apm.bob_rework.lib.entities.genBearing = function (name, base, ball, tint)
         }
 
     data:extend({recipe})
+
+    if handmade then
+        local recipe = {}
+        recipe.type = "recipe"
+        recipe.name = name.."-handmade"
+        recipe.category = 'apm_handcrafting_only'
+        recipe.normal = {}
+        recipe.normal.enabled = true
+        recipe.normal.energy_required = 1
+        recipe.normal.ingredients = {
+                {type="item", name=base, amount=2},
+                {type="item", name=apm.bob_rework.lib.entities.wood, amount=2}
+            }
+        recipe.normal.results = { 
+                {type='item', name=name, amount=12},
+                {type='item', name='apm_wood_pellets', amount=6}
+            }
+        recipe.normal.main_product = name
+        recipe.normal.requester_paste_multiplier = 4
+        recipe.normal.always_show_products = true
+        recipe.normal.always_show_made_in = true
+        recipe.expensive = table.deepcopy(recipe.normal)
+        recipe.expensive.ingredients = {
+                {type="item", name=base, amount=2}
+            }
+    
+        data:extend({recipe})
+    end
 end
 
 apm.bob_rework.lib.entities.genBearing(
     apm.bob_rework.lib.entities.bronzeBearing, apm.bob_rework.lib.entities.bronze,
-    apm.bob_rework.lib.entities.bronzeBearingBall, {r=151/255, g=115/255, b=81/255}
+    apm.bob_rework.lib.entities.bronzeBearingBall, {r=151/255, g=115/255, b=81/255},
+    true
 )
 apm.bob_rework.lib.entities.genBearing(
     apm.bob_rework.lib.entities.brassBearing, apm.bob_rework.lib.entities.brass,
-    apm.bob_rework.lib.entities.brassBearingBall, {r=235/255, g=244/255, b=181/255}
+    apm.bob_rework.lib.entities.brassBearingBall, {r=235/255, g=244/255, b=181/255},
+    false
 )
