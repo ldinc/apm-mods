df63c899636e852e87763ccf2aacf0c4181e8458
ldinc <drogunov.igor@gmail.com>
2024-11-02 17:32:54 +0300

    apm_power: inline tank, steam engine, pump migration

--- apm_power/prototypes/main/entities/inline_storage_tank.lua
+++ apm_power/prototypes/main/entities/inline_storage_tank.lua
@@ -5,27 +5,24 @@ local inline_storage_tank = require('graphics.inline_storage_tank')
 local sprites = inline_storage_tank
 
 local empty = {
-    filename = "__core__/graphics/empty.png",
-        priority = "extra-high",
-        width = 1,
-        height = 1
+	filename = "__core__/graphics/empty.png",
+	priority = "extra-high",
+	width = 1,
+	height = 1
 }
-
-local ppPath = '__apm_resource_pack_ldinc__/graphics/entities/'
 
 local pipe_picture =
 {
-    north = empty,
-    east = empty,
-    south = sprites.pipe_picture.south,
-    west = empty
+	north = empty,
+	east = empty,
+	south = sprites.pipe_picture.south,
+	west = empty
 }
-
 
 local tank = {}
 tank.type = "storage-tank"
 tank.name = "apm_inline_storage_tank"
-tank.icons = {sprites.icon}
+tank.icons = { sprites.icon }
 tank.localised_description = { "entity-description.apm_inline_storage_tank" }
 --sinkhole.icon_size = 32
 tank.flags = { "placeable-neutral", "placeable-player", "player-creation" }
@@ -49,56 +46,56 @@ tank.collision_box = { { -0.45, -0.45 }, { 0.45, 0.45 } }
 tank.selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } }
 
 local pic = {
-    layers = {
-        {
-            filename = sprites.base.lr.path,
-            priority = "high",
-            width = sprites.base.lr.w,
-            height = sprites.base.lr.h,
-            scale = sprites.base.lr.scale,
-            shift = sprites.base.lr.shift,
-            hr_version = {
-                filename = sprites.base.hr.path,
-                priority = "high",
-                width = sprites.base.hr.w,
-                height = sprites.base.hr.h,
-                scale = sprites.base.hr.scale,
-                shift = sprites.base.hr.shift,
-            },
-        },
-        {
-            filename = sprites.shadow.lr.path,
-            priority = "high",
-            width = sprites.shadow.lr.w,
-            height = sprites.shadow.lr.h,
-            scale = sprites.shadow.lr.scale,
-            shift = sprites.shadow.lr.shift,
-            draw_as_shadow = true,
-            hr_version = {
-                filename = sprites.shadow.hr.path,
-                priority = "high",
-                width = sprites.shadow.hr.w,
-                height = sprites.shadow.hr.h,
-                scale = sprites.shadow.hr.scale,
-                shift = sprites.shadow.hr.shift,
-                draw_as_shadow = true,
-            },
-        },
-    },
+	layers = {
+		{
+			filename = sprites.base.lr.path,
+			priority = "high",
+			width = sprites.base.lr.w,
+			height = sprites.base.lr.h,
+			scale = sprites.base.lr.scale,
+			shift = sprites.base.lr.shift,
+			hr_version = {
+				filename = sprites.base.hr.path,
+				priority = "high",
+				width = sprites.base.hr.w,
+				height = sprites.base.hr.h,
+				scale = sprites.base.hr.scale,
+				shift = sprites.base.hr.shift,
+			},
+		},
+		{
+			filename = sprites.shadow.lr.path,
+			priority = "high",
+			width = sprites.shadow.lr.w,
+			height = sprites.shadow.lr.h,
+			scale = sprites.shadow.lr.scale,
+			shift = sprites.shadow.lr.shift,
+			draw_as_shadow = true,
+			hr_version = {
+				filename = sprites.shadow.hr.path,
+				priority = "high",
+				width = sprites.shadow.hr.w,
+				height = sprites.shadow.hr.h,
+				scale = sprites.shadow.hr.scale,
+				shift = sprites.shadow.hr.shift,
+				draw_as_shadow = true,
+			},
+		},
+	},
 }
 
 tank.pictures = {
-    fluid_background  = empty,
-    window_background = empty,
-    flow_sprite = empty,
-    gas_flow = empty,
+	fluid_background  = empty,
+	window_background = empty,
+	flow_sprite       = empty,
+	gas_flow          = empty,
 
-    picture = {
-        north = pic,
-        east = pic,
-        south = pic,
-        west = pic,
-    }
+	picture           = {
+		north = pic,
+		east = pic,
+		south = pic,
+		west = pic,
+	}
 }
 
 
@@ -114,39 +111,39 @@ tank.fluid_box.filter = ""
 -- tank.fluid_box.pipe_covers = pipecoverspictures()
 tank.hide_connection_info = true
 tank.fluid_box.pipe_connections = {
-    {
-        type = "input-output",
-        position = { 0, 1 }
-    },
-    {
-        type = "input-output",
-        position = {0, -1},
-    },
-    {
-        type = "input-output",
-        position = { 1, 0 }
-    },
-    {
-        type = "input-output",
-        position = {-1, 0},
-    }
+	{
+		type = "input-output",
+		position = { 0, 1 }
+	},
+	{
+		type = "input-output",
+		position = { 0, -1 },
+	},
+	{
+		type = "input-output",
+		position = { 1, 0 }
+	},
+	{
+		type = "input-output",
+		position = { -1, 0 },
+	}
 }
 -- tank.fluid_box.secondary_draw_orders = { north = -1, south = 1, west = -1, east = 1 }
 
-tank.window_bounding_box = {util.by_pixel(-3, -5), util.by_pixel(3, 11)}
+tank.window_bounding_box = { util.by_pixel(-3, -5), util.by_pixel(3, 11) }
 
 tank.flow_length_in_ticks = 360
 
-circuit_connector_definitions["apm_inline_storage_tank"] = circuit_connector_definitions.create
-(
-  universal_connector_template,
-  {
-    { variation = 27, main_offset = util.by_pixel(10, -18), shadow_offset = util.by_pixel(6, -16), show_shadow = false },
-    { variation = 25, main_offset = util.by_pixel(-10, -18), shadow_offset = util.by_pixel(-6, -16), show_shadow = false },
-    { variation = 27, main_offset = util.by_pixel(10, -18), shadow_offset = util.by_pixel(6, -16), show_shadow = false },
-    { variation = 25, main_offset = util.by_pixel(-10, -18), shadow_offset = util.by_pixel(-6, -16), show_shadow = false }
-  }
-)
+circuit_connector_definitions["apm_inline_storage_tank"] = circuit_connector_definitions.create_vector
+	(
+		universal_connector_template,
+		{
+			{ variation = 27, main_offset = util.by_pixel(10, -18),  shadow_offset = util.by_pixel(6, -16),  show_shadow = false },
+			{ variation = 25, main_offset = util.by_pixel(-10, -18), shadow_offset = util.by_pixel(-6, -16), show_shadow = false },
+			{ variation = 27, main_offset = util.by_pixel(10, -18),  shadow_offset = util.by_pixel(6, -16),  show_shadow = false },
+			{ variation = 25, main_offset = util.by_pixel(-10, -18), shadow_offset = util.by_pixel(-6, -16), show_shadow = false }
+		}
+	)
 
 tank.circuit_wire_connection_points = circuit_connector_definitions[tank.name].points
 tank.circuit_connector_sprites = circuit_connector_definitions[tank.name].sprites
--- apm_power/prototypes/main/entities/steam_engines.lua
+++ apm_power/prototypes/main/entities/steam_engines.lua
@@ -17,23 +17,24 @@ APM_LOG_HEADER(self)
 local steam_engine = table.deepcopy(data.raw.generator['steam-engine'])
 steam_engine.name = 'apm_steam_engine_2'
 steam_engine.icons = {
-    apm.power.icons.steam_engine,
-    apm.lib.icons.dynamics.t2
+	apm.power.icons.steam_engine,
+	apm.lib.icons.dynamics.t2
 }
---steam_engine.icon_size = apm.power.icons.steam_engine.icon_size
-steam_engine.minable = {mining_time = 0.2, result = "apm_steam_engine_2"}
+
+steam_engine.minable = { mining_time = 0.2, result = "apm_steam_engine_2" }
 steam_engine.fast_replaceable_group = 'steam-engine'
 steam_engine.max_health = 500
 steam_engine.maximum_temperature = 240
 steam_engine.fluid_usage_per_tick = 0.45
+
 steam_engine.horizontal_animation.layers[3] = table.deepcopy(steam_engine.horizontal_animation.layers[1])
-steam_engine.horizontal_animation.layers[3].filename = '__apm_resource_pack_ldinc__/graphics/masks/steam_engine/steam-engine-H.png'
+steam_engine.horizontal_animation.layers[3].filename =
+'__apm_resource_pack_ldinc__/graphics/masks/steam_engine/hr-steam-engine-H.png'
 steam_engine.horizontal_animation.layers[3].tint = apm.power.color.steam_engine_tier_2
-steam_engine.horizontal_animation.layers[3].hr_version.filename = '__apm_resource_pack_ldinc__/graphics/masks/steam_engine/hr-steam-engine-H.png'
-steam_engine.horizontal_animation.layers[3].hr_version.tint = apm.power.color.steam_engine_tier_2
+
 steam_engine.vertical_animation.layers[3] = table.deepcopy(steam_engine.vertical_animation.layers[1])
-steam_engine.vertical_animation.layers[3].filename = '__apm_resource_pack_ldinc__/graphics/masks/steam_engine/steam-engine-V.png'
+steam_engine.vertical_animation.layers[3].filename =
+'__apm_resource_pack_ldinc__/graphics/masks/steam_engine/hr-steam-engine-V.png'
 steam_engine.vertical_animation.layers[3].tint = apm.power.color.steam_engine_tier_2
-steam_engine.vertical_animation.layers[3].hr_version.filename = '__apm_resource_pack_ldinc__/graphics/masks/steam_engine/hr-steam-engine-V.png'
-steam_engine.vertical_animation.layers[3].hr_version.tint = apm.power.color.steam_engine_tier_2
-data:extend({steam_engine})
+
+data:extend({ steam_engine })
