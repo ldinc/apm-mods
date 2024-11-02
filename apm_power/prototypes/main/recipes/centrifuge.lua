cb9bd5f05e97b2968a367d054d31668207a27143
ldinc <drogunov.igor@gmail.com>
2024-11-03 01:54:31 +0300

    apm_power: fix fluid heat cap KJ->kJ

--- apm_power/prototypes/main/recipes/centrifuge.lua
+++ apm_power/prototypes/main/recipes/centrifuge.lua
@@ -14,9 +14,9 @@ APM_LOG_SETTINGS(self, 'apm_power_always_show_made_in', apm_power_always_show_ma
 -- ----------------------------------------------------------------------------
 local item_icon_a = apm.lib.utils.icon.get.from_fluid('apm_sea_water')
 local item_icon_b = apm.lib.utils.icon.get.from_fluid('water')
-item_icon_b = apm.lib.utils.icons.mod(item_icon_b, 0.5, {-8,8})
-local item_icon_c = {apm.lib.icons.dynamics.recycling}
-local icons = apm.lib.utils.icon.merge({item_icon_a, item_icon_b, item_icon_c})
+item_icon_b = apm.lib.utils.icons.mod(item_icon_b, 0.5, { -8, 8 })
+local item_icon_c = { apm.lib.icons.dynamics.recycling }
+local icons = apm.lib.utils.icon.merge({ item_icon_a, item_icon_b, item_icon_c })
 
 local recipe = {}
 recipe.type = "recipe"
@@ -30,19 +30,19 @@ recipe.icons = icons
 recipe.enabled = false
 recipe.energy_required = 2
 recipe.ingredients = {
-		{type="fluid", name="apm_sea_water", amount=200}
-	}
-recipe.results = { 
-		{type="item", name="apm_wet_mud", amount_min=1, amount_max=1, probability=0.5},
-		{type="fluid", name="water", amount=195},
-		{type="item", name="raw-fish", amount_min=1, amount_max=1, probability=0.0001, show_details_in_recipe_tooltip=false}
-	}
+	{ type = "fluid", name = "apm_sea_water", amount = 200 }
+}
+recipe.results = {
+	{ type = "item", name = "apm_wet_mud", amount_min = 1, amount_max = 1, probability = 0.5 },
+	{ type = "fluid", name = "water",   amount = 195 },
+	{ type = "item", name = "raw-fish", amount_min = 1, amount_max = 1, probability = 0.0001, show_details_in_recipe_tooltip = false }
+}
 recipe.main_product = ''
 recipe.requester_paste_multiplier = 4
 recipe.always_show_products = true
 recipe.always_show_made_in = apm_power_always_show_made_in
 
-data:extend({recipe})
+data:extend({ recipe })
 
 -- Recipe ---------------------------------------------------------------------
 --
@@ -50,9 +50,9 @@ data:extend({recipe})
 -- ----------------------------------------------------------------------------
 local item_icon_a = apm.lib.utils.icon.get.from_fluid('apm_dirt_water')
 local item_icon_b = apm.lib.utils.icon.get.from_fluid('water')
-item_icon_b = apm.lib.utils.icons.mod(item_icon_b, 0.5, {-8,8})
-local item_icon_c = {apm.lib.icons.dynamics.recycling}
-local icons = apm.lib.utils.icon.merge({item_icon_a, item_icon_b, item_icon_c})
+item_icon_b = apm.lib.utils.icons.mod(item_icon_b, 0.5, { -8, 8 })
+local item_icon_c = { apm.lib.icons.dynamics.recycling }
+local icons = apm.lib.utils.icon.merge({ item_icon_a, item_icon_b, item_icon_c })
 
 local recipe = {}
 recipe.type = "recipe"
@@ -66,12 +66,12 @@ recipe.icons = icons
 recipe.enabled = false
 recipe.energy_required = 5.5
 recipe.ingredients = {
-		{type="fluid", name="apm_dirt_water", amount=200}
-	}
-recipe.results = { 
-		{type="fluid", name="water", amount=180},
-		apm.lib.utils.builder.recipe.item.simple('APM_SLAG', 2, 0, 5),
-	}
+	{ type = "fluid", name = "apm_dirt_water", amount = 200 }
+}
+recipe.results = {
+	{ type = "fluid", name = "water", amount = 180 },
+	apm.lib.utils.builder.recipe.item.simple('APM_SLAG', 2, 0, 5),
+}
 recipe.main_product = ''
 recipe.requester_paste_multiplier = 4
 recipe.always_show_products = true
@@ -80,7 +80,7 @@ recipe.allow_decomposition = false
 recipe.allow_as_intermediate = false
 recipe.allow_intermediates = false
 
-data:extend({recipe})
+data:extend({ recipe })
 
 -- Recipe ---------------------------------------------------------------------
 --
@@ -88,9 +88,9 @@ data:extend({recipe})
 -- ----------------------------------------------------------------------------
 local item_icon_a = apm.lib.utils.icon.get.from_fluid('apm_coal_saturated_wastewater')
 local item_icon_b = apm.lib.utils.icon.get.from_fluid('water')
-item_icon_b = apm.lib.utils.icons.mod(item_icon_b, 0.5, {-8,8})
-local item_icon_c = {apm.lib.icons.dynamics.recycling}
-local icons = apm.lib.utils.icon.merge({item_icon_a, item_icon_b, item_icon_c})
+item_icon_b = apm.lib.utils.icons.mod(item_icon_b, 0.5, { -8, 8 })
+local item_icon_c = { apm.lib.icons.dynamics.recycling }
+local icons = apm.lib.utils.icon.merge({ item_icon_a, item_icon_b, item_icon_c })
 
 local recipe = {}
 recipe.type = "recipe"
@@ -104,12 +104,12 @@ recipe.icons = icons
 recipe.enabled = false
 recipe.energy_required = 6.5
 recipe.ingredients = {
-		{type="fluid", name="apm_coal_saturated_wastewater", amount=100}
-	}
-recipe.results = { 
-		{type="fluid", name="water", amount=80},
-		{type="item", name="coal", amount=2, show_details_in_recipe_tooltip=false}
-	}
+	{ type = "fluid", name = "apm_coal_saturated_wastewater", amount = 100 }
+}
+recipe.results = {
+	{ type = "fluid", name = "water", amount = 80 },
+	{ type = "item", name = "coal", amount = 2, show_details_in_recipe_tooltip = false }
+}
 recipe.main_product = ''
 recipe.requester_paste_multiplier = 4
 recipe.always_show_products = true
@@ -118,4 +118,4 @@ recipe.allow_decomposition = false
 recipe.allow_as_intermediate = false
 recipe.allow_intermediates = false
 
-data:extend({recipe})
+data:extend({ recipe })
