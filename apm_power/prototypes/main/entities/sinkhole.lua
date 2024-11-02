13f606665a4b4457d4522693570e754a90aed690
ldinc <drogunov.igor@gmail.com>
2024-11-03 02:33:20 +0300

    apm_power: update sinkhole

--- apm_power/prototypes/main/entities/sinkhole.lua
+++ apm_power/prototypes/main/entities/sinkhole.lua
@@ -1,6 +1,10 @@
 require('util')
 require('__apm_lib_ldinc__.lib.log')
 
+local sounds = require("__base__.prototypes.entity.sounds")
+
+local hit_effects = require("__base__.prototypes.entity.hit-effects")
+
 local self = 'apm_power/prototypes/main/sinkhole.lua'
 
 APM_LOG_HEADER(self)
@@ -9,21 +13,21 @@ local ppPath = "__apm_resource_pack_ldinc__/graphics/entities/sinkhole/"
 
 local empty = {
     filename = "__core__/graphics/empty.png",
-        priority = "extra-high",
-        width = 1,
-        height = 1
+    priority = "extra-high",
+    width = 1,
+    height = 1
 }
 
 local pipe_picture =
 {
     north = {
-        filename = ppPath.."pipecovers/pipe-N.png",
+        filename = ppPath .. "pipecovers/pipe-N.png",
         priority = "extra-high",
         width = 35,
         height = 18,
         shift = util.by_pixel(2.5, 14),
         hr_version = {
-            filename = ppPath.."pipecovers/hr-pipe-N.png",
+            filename = ppPath .. "pipecovers/hr-pipe-N.png",
             priority = "extra-high",
             width = 71,
             height = 38,
@@ -33,13 +37,13 @@ local pipe_picture =
     },
     east = empty,
     south = {
-        filename = ppPath.."pipecovers/pipe-S.png",
+        filename = ppPath .. "pipecovers/pipe-S.png",
         priority = "extra-high",
         width = 44,
         height = 31,
         shift = util.by_pixel(0, -31.5),
         hr_version = {
-            filename = ppPath.."pipecovers/hr-pipe-S.png",
+            filename = ppPath .. "pipecovers/hr-pipe-S.png",
             priority = "extra-high",
             width = 88,
             height = 61,
@@ -57,7 +61,7 @@ pipe_covers.west = empty
 local sinkhole = {}
 sinkhole.type = "furnace"
 sinkhole.name = "apm_sinkhole"
-sinkhole.icons = {apm.lib.icons.static.sinkhole}
+sinkhole.icons = { apm.lib.icons.static.sinkhole }
 sinkhole.localised_description = { "entity-description.apm_sinkhole" }
 --sinkhole.icon_size = 32
 sinkhole.flags = { "placeable-neutral", "placeable-player", "player-creation" }
@@ -65,11 +69,26 @@ sinkhole.minable = { mining_time = 0.2, result = "apm_sinkhole" }
 sinkhole.max_health = 400
 sinkhole.corpse = "medium-remnants"
 sinkhole.dying_explosion = "medium-explosion"
+
+sinkhole.impact_category = "metal"
+sinkhole.working_sound =
+{
+    sound = {
+        filename = "__base__/sound/offshore-pump.ogg",
+        volume = 0.5,
+    },
+    match_volume_to_activity = true,
+    audible_distance_modifier = 0.7,
+    max_sounds_per_type = 3,
+    fade_in_ticks = 4,
+    fade_out_ticks = 20
+}
 sinkhole.repair_sound = { filename = "__base__/sound/manual-repair-simple.ogg" }
 sinkhole.mined_sound = { filename = "__base__/sound/deconstruct-bricks.ogg" }
 sinkhole.open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 }
 sinkhole.close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 }
 sinkhole.vehicle_impact_sound = { filename = "__base__/sound/car-stone-impact.ogg", volume = 1.0 }
+
 sinkhole.source_inventory_size = 0
 sinkhole.result_inventory_size = 0
 sinkhole.next_upgrade = nil
@@ -84,7 +103,7 @@ sinkhole.selection_box = { { -1.5, -1.5 }, { 1.5, 1.5 } }
 sinkhole.energy_usage = apm.power.constants.energy_usage.electric
 sinkhole.energy_source = {
     type = "void",
-    emissions_per_minute = 5,
+    emissions_per_minute = { pollution = 5 },
     fluid_box = {}
 }
 
@@ -97,47 +116,48 @@ sinkhole.energy_source = {
 sinkhole.module_specification = apm.power.constants.modules.specification_2
 sinkhole.allowed_effects = apm.power.constants.modules.allowed_effects_2
 
+sinkhole.resistances = {
+    {
+        type = "fire",
+        percent = 90
+    },
+    {
+        type = "explosion",
+        percent = 40
+    },
+    {
+        type = "impact",
+        percent = 30
+    }
+}
 
+sinkhole.damaged_trigger_effect = hit_effects.rock()
 
-sinkhole.animation = {}
-sinkhole.animation.layers = {}
-sinkhole.animation.layers[1] = {}
-sinkhole.animation.layers[1].filename = ppPath.."sinkhole.png"
-sinkhole.animation.layers[1].priority = "high"
-sinkhole.animation.layers[1].width = 192
-sinkhole.animation.layers[1].height = 96
-sinkhole.animation.layers[1].frame_count = 1
-sinkhole.animation.layers[1].line_length = 1
-sinkhole.animation.layers[1].hr_version = {}
-sinkhole.animation.layers[1].hr_version.shift = { 0, 0 }
-sinkhole.animation.layers[1].hr_version.filename = ppPath.."hr-sinkhole.png"
-sinkhole.animation.layers[1].hr_version.priority = "high"
-sinkhole.animation.layers[1].hr_version.width = 384
-sinkhole.animation.layers[1].hr_version.height = 192
-sinkhole.animation.layers[1].hr_version.frame_count = 1
-sinkhole.animation.layers[1].hr_version.line_length = 1
-sinkhole.animation.layers[1].hr_version.shift = { 0, 0 }
-sinkhole.animation.layers[1].hr_version.scale = 0.5
-
-sinkhole.animation.layers[2] = {}
-sinkhole.animation.layers[2].filename = ppPath.."sinkhole-shadow.png"
-sinkhole.animation.layers[2].priority = "high"
-sinkhole.animation.layers[2].draw_as_shadow = true
-sinkhole.animation.layers[2].width = 192
-sinkhole.animation.layers[2].height = 96
-sinkhole.animation.layers[2].frame_count = 1
-sinkhole.animation.layers[2].line_length = 1
-sinkhole.animation.layers[2].shift = { 0, 0 }
-sinkhole.animation.layers[2].hr_version = {}
-sinkhole.animation.layers[2].hr_version.filename = ppPath.."hr-sinkhole-shadow.png"
-sinkhole.animation.layers[2].hr_version.priority = "high"
-sinkhole.animation.layers[2].hr_version.draw_as_shadow = true
-sinkhole.animation.layers[2].hr_version.width = 384
-sinkhole.animation.layers[2].hr_version.height = 192
-sinkhole.animation.layers[2].hr_version.frame_count = 1
-sinkhole.animation.layers[2].hr_version.line_length = 1
-sinkhole.animation.layers[2].hr_version.shift = { 0, 0 }
-sinkhole.animation.layers[2].hr_version.scale = 0.5
+sinkhole.graphics_set = {
+    animation = {
+        layers = {
+            {
+                filename = ppPath .. "hr-sinkhole.png",
+                priority = "extra-high",
+                width = 384,
+                height = 192,
+                frame_count = 1,
+                line_length = 1,
+                scale = 0.5,
+            },
+            {
+                filename = ppPath .. "hr-sinkhole-shadow.png",
+                priority = "extra-high",
+                draw_as_shadow = true,
+                width = 384,
+                height = 192,
+                frame_count = 1,
+                line_length = 1,
+                scale = 0.5,
+            }
+        },
+    },
+}
 
 local base_area = settings.startup["apm_sinkhole_fluid_rate"].value / 10
 
@@ -149,10 +169,12 @@ sinkhole.fluid_boxes[1].pipe_covers = pipe_covers
 sinkhole.fluid_boxes[1].base_area = base_area
 sinkhole.fluid_boxes[1].base_level = -1
 sinkhole.fluid_boxes[1].filter = ""
+sinkhole.fluid_boxes[1].volume = 1000
 sinkhole.fluid_boxes[1].pipe_connections = {
     {
-        type = "input",
-        position = { 0, 2 }
+        flow_direction = "input",
+        direction = defines.direction.south,
+        position = { 0, 1.445 }
     }
 }
 sinkhole.fluid_boxes[1].secondary_draw_orders = { north = -1 }
