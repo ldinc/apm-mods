ed9e893ed21d13fcb1cae8f2ededc72cf5614ac0
ldinc <drogunov.igor@gmail.com>
2025-05-01 20:15:03 +0300

    migrating builder to goft tool

--- reload.bat
+++ reload.bat
@@ -10,18 +10,22 @@ if %target%==unstable (
   set branch=unstable
 )
 
-echo target %target%
-echo release %branch%
+@REM echo target %target%
+@REM echo release %branch%
 
-call build.bat %target%
+set mod=%target%
+
+@REM call build.bat %target%
 
 set mp=.factorio.%branch%
 
 
 for /f "tokens=* delims=" %%# in ('jq -r %mp% build.config.json') do @(set target=%%#)
 
-echo copy to %target%
-move %modname%_%version%.zip %target%mods\
+@REM echo copy to %target%
+@REM move %modname%_%version%.zip %target%mods\
+
+goft.exe -b -o %target%mods %mod% 
 
 @REM @REM start steam://rungameid/427520
 call %target%bin\x64\factorio.exe
