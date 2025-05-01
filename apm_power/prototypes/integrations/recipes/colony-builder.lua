ab300ff6b1d52ad417221475c7bdb77384855683
ldinc <drogunov.igor@gmail.com>
2025-05-01 23:07:34 +0300

    [apm_power] [colony builder] compat gh recipe

--- apm_power/prototypes/integrations/recipes/colony-builder.lua
+++ apm_power/prototypes/integrations/recipes/colony-builder.lua
@@ -6,3 +6,24 @@ apm.lib.utils.recipe.ingredient.remove_all(recipe_name)
 apm.lib.utils.recipe.ingredient.mod(recipe_name, "water", 12000)
 apm.lib.utils.recipe.ingredient.mod(recipe_name, "apm_wet_mud", 200)
 apm.lib.utils.recipe.ingredient.mod(recipe_name, "apm_crushed_stone", 50)
+
+
+local apm_power_overhaul_machine_frames = settings.startup["apm_power_overhaul_machine_frames"].value
+
+recipe_name = "colony-greenhouse"
+
+apm.lib.utils.recipe.ingredient.remove_all(recipe_name)
+apm.lib.utils.recipe.ingredient.mod(recipe_name, "electric-engine-unit", 25)
+apm.lib.utils.recipe.ingredient.mod(recipe_name, "refined-concrete", 50)
+apm.lib.utils.recipe.ingredient.mod(recipe_name, "small-lamp", 50)
+apm.lib.utils.recipe.ingredient.mod(recipe_name, "processing-unit", 50)
+
+if apm_power_overhaul_machine_frames then
+	apm.lib.utils.recipe.ingredient.mod(recipe_name, "apm_machine_frame_advanced", 12)
+end
+
+local apm_power_compat_bob = settings.startup["apm_power_compat_bob"].value
+
+if mods.bobplates  and apm_power_compat_bob then
+	apm.lib.utils.recipe.ingredient.mod(recipe_name, "bob-glass", 100)
+end
