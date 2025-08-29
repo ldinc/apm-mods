8980db8345922f67023f442b5396adff22fdcaab
ldinc <drogunov.igor@gmail.com>
2025-08-29 21:07:58 +0300

    [apm] copy.bat add all option

--- copy.bat
+++ copy.bat
@@ -16,4 +16,9 @@ set mp=.factorio.%branch%
 
 for /f "tokens=* delims=" %%# in ('jq -r %mp% build.config.json') do @(set target=%%#)
 
-goft.exe -b -o %target%mods %mod%
+if %mod%==all (
+  goft.exe -b -o %target%mods apm_lib apm_nuclear apm_power apm_resource_pack apm_energy_addon
+) else (
+  goft.exe -b -o %target%mods %mod%
+)
+
